14eb54148ec924c241001bb65c4d33d1cfe7f753
Orkun Duman <orkun1675@gmail.com>
2024-10-21 12:05:05 -0400

    Update deps

--- lib/firebase_auth_games_services.dart
+++ lib/firebase_auth_games_services.dart
@@ -1,5 +1,3 @@
-library firebase_auth_games_services;
-
 export 'src/firebase_auth_extension.dart';
 export 'src/firebase_auth_games_services_exception.dart';
 export 'src/firebase_auth_games_services.dart';
